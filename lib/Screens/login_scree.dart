623103cfc353b8c90be4ea53d55da6fa009fb8b1
Piyushbhatt7 <vengersmon003@gmail.com>
2025-05-05 12:28:38 +0530

    login screen

---
+++ lib/Screens/login_scree.dart
@@ -0,0 +1,13 @@
+class LoginScree extends StatefulWidget {
+  const LoginScree({super.key});
+
+  @override
+  State<LoginScree> createState() => _LoginScreeState();
+}
+
+class _LoginScreeState extends State<LoginScree> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
