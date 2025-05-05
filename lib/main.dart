459912e38d820420fb7bfb22d00e4047535494b9
Piyushbhatt7 <vengersmon003@gmail.com>
2025-05-05 12:29:08 +0530

    login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_docs/Screens/login_scree.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
         
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const  
+      home: const LoginScree(), 
     );
   }
 }
