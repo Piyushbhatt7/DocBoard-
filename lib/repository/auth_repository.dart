5050caa84e2a409663143abd57c9db34da26a24d
Piyushbhatt7 <vengersmon003@gmail.com>
2025-05-06 21:04:01 +0530

    auth rep

--- lib/repository/auth_repository.dart
+++ lib/repository/auth_repository.dart
@@ -2,7 +2,9 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 final authRepositoryProvider = Provider(
-  (ref) => AuthRepository(googleSignIn: GoogleSignIn()));
+  (ref) => AuthRepository(
+    googleSignIn: GoogleSignIn()
+    ));
 class AuthRepository {
   final GoogleSignIn _googleSignIn;
   final provider
