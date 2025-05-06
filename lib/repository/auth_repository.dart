00adbe85a3a799376f9a07a1e6bf0dd416260e78
Piyushbhatt7 <vengersmon003@gmail.com>
2025-05-06 20:56:37 +0530

    auth rep

--- lib/repository/auth_repository.dart
+++ lib/repository/auth_repository.dart
@@ -3,11 +3,10 @@ import 'package:google_sign_in/google_sign_in.dart';
 class AuthRepository {
   final GoogleSignIn _googleSignIn;
 
-  AuthRepository({required GoogleSignIn googleSignIn}) : _googleSignIn = googleSignIn;
+  AuthRepository({required GoogleSignIn googleSignIn})
+    : _googleSignIn = googleSignIn;
 
   void signInWithGoogle() async {
-
-     
+    
   }
- 
-}                                         
+}
