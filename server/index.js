642073f9d1846c0d566d5fbc4b66e778e18eddf3
Piyushbhatt7 <vengersmon003@gmail.com>
2025-05-10 22:13:23 +0530

    index js file

--- server/index.js
+++ server/index.js
@@ -8,12 +8,14 @@ const app = express();
 
 const DB = "mongodb+srv://piyushbhatt162:0k0P2FcurljxejUR@cluster0.mrjbpoh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-mongoose.connect(DB).then(() => {
+mongoose.connect(DB)
+.then(() => {
     console.log("Connection successful !");
-}).catch((err) => {
+})
+.catch((err) => {
     console.log(err);
 
-})
+});
 
 // async -> await
 // .then((ref) => print(ref)
