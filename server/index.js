6686e02dc8eb620ffd84c7f6cd203e296ca70456
Piyushbhatt7 <vengersmon003@gmail.com>
2025-05-10 21:35:02 +0530

    index file

--- server/index.js
+++ server/index.js
@@ -6,6 +6,6 @@ const PORT = process.env.PORT | 3001;
 
 const app = express();
 
-app.listen(PORT, "0.0.0.0", function() {
+app.listen(PORT, "0.0.0.0", () => {
     console.log('connected at port 3001')
 })
