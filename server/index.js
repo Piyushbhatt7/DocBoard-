cdaa133d16bf9ff589bcd951da206ab81f1928f0
Piyushbhatt7 <vengersmon003@gmail.com>
2025-05-10 21:53:36 +0530

    index file

--- server/index.js
+++ server/index.js
@@ -6,6 +6,8 @@ const PORT = process.env.PORT | 3001;
 
 const app = express();
 
+mongoose.connect()
+
 app.listen(PORT, "0.0.0.0", () => {
     console.log('connected at port 3001');
     console.log("hey this is changing");
