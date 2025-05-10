13e61dd5dfa78f35ae685897f3d49acf15602aca
Piyushbhatt7 <vengersmon003@gmail.com>
2025-05-10 21:44:40 +0530

    writing script to run nodemon

--- server/index.js
+++ server/index.js
@@ -7,5 +7,6 @@ const PORT = process.env.PORT | 3001;
 const app = express();
 
 app.listen(PORT, "0.0.0.0", () => {
-    console.log('connected at port 3001')
+    console.log('connected at port 3001');
+    console.log("hey this is changing");
 })
