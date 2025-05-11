155fff563e5444f5c996a5e94ad9143637ec1e2b
Piyushbhatt7 <vengersmon003@gmail.com>
2025-05-11 15:49:20 +0530

    user model

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -3,6 +3,15 @@ class UserModel {
   final String id;
   final String name;
   final String profilePic;
+  final String uid;
   final String token;
+
+  UserModel({
+    required this.id, 
+    required this.name, 
+    required this.profilePic, 
+    required this.uid, 
+    required this.token
+    });
 
 }
