85598e7e86759a455bbb3f7b867f86584ec6b982
Piyushbhatt7 <vengersmon003@gmail.com>
2025-05-11 12:47:58 +0530

    user model class

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -7,7 +7,10 @@ authRouter.post('api/sinup', async (req, res) => {
     try {
         const {name, email, profilPic} = req.body; // 1:10
         
-
+        // email already exists?
+        
+        // store data
+        // send response
     }
 
     catch (e)
