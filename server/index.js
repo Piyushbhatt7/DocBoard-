e2a2e46f0e2c198ec6fbaf6dd9863183386d9278
Piyushbhatt7 <vengersmon003@gmail.com>
2025-05-12 10:35:25 +0530

    cors dependencies

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 console.log('Hello, World')
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require('cors');
 const authRouter = require("./routes/auth");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.use(cors);
 app.use(express.json());
 app.use("/api", authRouter);
 
