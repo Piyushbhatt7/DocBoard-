c7cb0b42399fb7dee2138fb5033ed54f033e61eb
Piyushbhatt7 <vengersmon003@gmail.com>
2025-05-12 23:03:34 +0530

    auth

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -6,7 +6,7 @@ const auth = async (req, res, next) => {
 
     try {   
         const token = req.header("x-auth-token");       
- 
+  
         if(!token)
         {
             return res.status(401).json({msg: "No auth token access denied"});
