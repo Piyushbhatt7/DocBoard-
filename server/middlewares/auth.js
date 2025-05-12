612684f793b2a4bbc73c1e10014249a44ca93542
Piyushbhatt7 <vengersmon003@gmail.com>
2025-05-12 17:14:13 +0530

    "x-auth-token"

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,7 +1,16 @@
+const { json } = require("express");
+
 const auth = async (req, res, next) => {
 
     try {
-        const token = req.header()"x-auth-token";
+        const token = req.header("x-auth-token");
+
+        if(!token)
+        {
+            return res.status(401).json({msg: "No auth token access denied"});
+
+            const verified = 
+        }
     }
     catch(e)
     {
