4e649a409b1da680bd037f67b3b75e0dcb13372f
Piyushbhatt7 <vengersmon003@gmail.com>
2025-05-12 11:15:27 +0530

    login screen

--- lib/Screens/login_scree.dart
+++ lib/Screens/login_scree.dart
@@ -8,7 +8,6 @@ import 'package:google_docs/repository/auth_repository.dart';
 class LoginScree extends ConsumerStatefulWidget {
   const LoginScree ({Key? key}) : super(key: key);
 
-                   
   @override                                     
   ConsumerState<LoginScree> createState() => _LoginScreeState();                                     
 }        
@@ -16,55 +15,43 @@ class LoginScree extends ConsumerStatefulWidget {
 class _LoginScreeState extends ConsumerState<LoginScree> {               
   
   void signInWithGoogle(WidgetRef ref, BuildContext context) async {
-
     final sMessanger = ScaffoldMessenger.of(context);
     final navigator = Navigator.of(context);
     final errorModel = await ref.read(authRepositoryProvider).signInWithGoogle();   
-    if(errorModel.error == null){
+
+    if (errorModel.error == null) {
       ref.read(userProvider.notifier).update((state) => errorModel.data);
-      navigator.push(
-        MaterialPageRoute(
-          builder: (context) => const HomeScreen()
-          )
+      navigator.pushReplacement( // Use pushReplacement to replace login screen
+        MaterialPageRoute(builder: (context) => const HomeScreen())
       );
-    } 
-    else {
-        sMessanger
-        .showSnackBar(
-        SnackBar(content: 
-        Text(errorModel.error!)
-      ));
+    } else {
+      sMessanger.showSnackBar(SnackBar(content: Text(errorModel.error!)));
     }         
   }
 
   @override
   Widget build(BuildContext context) {
-    ref.watch(authRepositoryProvider).signInWithGoogle();
     return Scaffold(
       body: Center(
         child: ElevatedButton.icon(
-          onPressed: () => signInWithGoogle(ref, context),      
-          icon: Image.asset( 
-            'assets/images/g-logo-2.png',      
-            height: 20,  
+          onPressed: () => signInWithGoogle(ref, context),
+          icon: Image.asset(
+            'assets/images/g-logo-2.png',
+            height: 20,
           ),
           label: const Text(
             "Sign in with Google", 
-            style: TextStyle(
-            color: kBlackColor
-          ),),
+            style: TextStyle(color: kBlackColor),
+          ),
           style: ElevatedButton.styleFrom(
             backgroundColor: kWhiteColor,
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(10),
-              side: const BorderSide(
-                color: Colors.grey,
-                width: 1.5
-              )
+              side: const BorderSide(color: Colors.grey, width: 1.5),
             ),
-           minimumSize: const Size(150, 50)
+            minimumSize: const Size(150, 50),
           ),
-          ),
+        ),
       ),
     );
   }
