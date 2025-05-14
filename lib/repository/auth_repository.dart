6ecef03ad2a80852d5acc5d0e7ef93981cf37766
Piyushbhatt7 <vengersmon003@gmail.com>
2025-05-14 17:30:41 +0530

    auth repo

--- lib/repository/auth_repository.dart
+++ lib/repository/auth_repository.dart
@@ -1,6 +1,4 @@
 import 'dart:convert';
-import 'dart:io' show Platform;
-import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_docs/constants.dart';
 import 'package:google_docs/models/error_model.dart';
@@ -11,115 +9,100 @@ import 'package:http/http.dart';
 
 final authRepositoryProvider = Provider(
   (ref) => AuthRepository(
-    googleSignIn: GoogleSignIn(), 
-    client: Client(), 
-    localStorageRepo: LocalStorageRepo(),
-    ));
+    googleSignIn: GoogleSignIn(),
+    client: Client(),
+    localStorageRepository: LocalStorageRepository(),
+  ),
+);
 
-final userProvider = StateProvider<UserModel?>((ref) => null); // 1:47
+final userProvider = StateProvider<UserModel?>((ref) => null);
 
 class AuthRepository {
   final GoogleSignIn _googleSignIn;
   final Client _client;
-  final LocalStorageRepo _localStorageRepo;
-  //35:44
-
+  final LocalStorageRepository _localStorageRepository;
   AuthRepository({
-    required GoogleSignIn googleSignIn, 
+    required GoogleSignIn googleSignIn,
     required Client client,
-    required LocalStorageRepo localStorageRepo, 
+    required LocalStorageRepository localStorageRepository,
   })  : _googleSignIn = googleSignIn,
         _client = client,
-        _localStorageRepo = localStorageRepo;
+        _localStorageRepository = localStorageRepository;
 
-
-Future<ErrorModel> signInWithGoogle() async {
-  ErrorModel error = ErrorModel(error: 'Some unexpected error occurred', data: null);
-
-  try {
-    GoogleSignInAccount? user;
-
-    if (kIsWeb) {
-      // Use only silent sign-in on web
-      user = await _googleSignIn.signInSilently();
-    } else {
-      user = await _googleSignIn.signInSilently();
-      user ??= await _googleSignIn.signIn();
-    }
-
-    if (user != null) {
-      final userAcc = UserModel(
-        email: user.email,
-        name: user.displayName!,
-        profilePic: user.photoUrl ?? '',
-        uid: '',
-        token: '',
-      );
-
-      var res = await _client.post(
-        Uri.parse('$host/api/signup'),
-        body: jsonEncode(userAcc.toJson()),
-        headers: {
-          'Content-Type': 'application/json; charset=UTF-8'
-        },
-      );
-
-      if (res.statusCode == 200) {
-        final newUser = userAcc.copyWith(
-          uid: jsonDecode(res.body)['user']['_id'],
-          token: jsonDecode(res.body)['token'],
+  Future<ErrorModel> signInWithGoogle() async {
+    ErrorModel error = ErrorModel(
+      error: 'Some unexpected error occurred.',
+      data: null,
+    );
+    try {
+      final user = await _googleSignIn.signIn();
+      if (user != null) {
+        final userAcc = UserModel(
+          email: user.email,
+          name: user.displayName ?? '',
+          profilePic: user.photoUrl ?? '',
+          uid: '',
+          token: '',
         );
-        error = ErrorModel(error: null, data: newUser);
-        await _localStorageRepo.setToken(newUser.token);
-      } else {
-        error = ErrorModel(
-            error: 'Signup failed. Status code: ${res.statusCode}', data: null);
+
+        var res = await _client.post(Uri.parse('$host/api/signup'), body: userAcc.toJson(), headers: {
+          'Content-Type': 'application/json; charset=UTF-8',
+        });
+
+        switch (res.statusCode) {
+          case 200:
+            final newUser = userAcc.copyWith(
+              uid: jsonDecode(res.body)['user']['_id'],
+              token: jsonDecode(res.body)['token'],
+            );
+            error = ErrorModel(error: null, data: newUser);
+            _localStorageRepository.setToken(newUser.token);
+            break;
+        }
       }
-    }
-  } catch (e) {
-    print('[SIGNIN ERROR] $e');
-    error = ErrorModel(error: e.toString(), data: null);
-  }
-
-  return error;
-}
-
-
- Future<ErrorModel> getUserData() async {
-  ErrorModel error = ErrorModel(error: 'Some unexpected error occurred', data: null);
-
-  try {
-    
-    String? token = await _localStorageRepo.getToken();
-
-    if(token!=null)
-    {
-
-      var res = await _client.get(
-        Uri.parse('$host/'),
-        headers: {
-          'Content-Type': 'application/json',
-          'x-auth-token': token,
-        },
+    } catch (e) {
+      error = ErrorModel(
+        error: e.toString(),
+        data: null,
       );
-
-      switch (res.statusCode) {
-        case 200:
-          final newUser = UserModel.fromJson(jsonDecode(res.body)['user'])
-          .copyWith(token: token);
-
-          error = ErrorModel(error: null, data: newUser);
-          _localStorageRepo.setToken(newUser.token);
-          break;
-      }
     }
-    
-  } catch (e) {
-    print(e);
-    error = ErrorModel(error: e.toString(), data: null);
+    return error;
   }
 
-  return error;
-}
+  Future<ErrorModel> getUserData() async {
+    ErrorModel error = ErrorModel(
+      error: 'Some unexpected error occurred.',
+      data: null,
+    );
+    try {
+      String? token = await _localStorageRepository.getToken();
 
+      if (token != null) {
+        var res = await _client.get(Uri.parse('$host/'), headers: {
+          'Content-Type': 'application/json; charset=UTF-8',
+          'x-auth-token': token,
+        });
+        switch (res.statusCode) {
+          case 200:
+            final newUser = UserModel.fromJson(
+              jsonDecode(res.body)['user'],
+            ).copyWith(token: token);
+            error = ErrorModel(error: null, data: newUser);
+            _localStorageRepository.setToken(newUser.token);
+            break;
+        }
+      }
+    } catch (e) {
+      error = ErrorModel(
+        error: e.toString(),
+        data: null,
+      );
+    }
+    return error;
+  }
+
+  void signOut() async {
+    await _googleSignIn.signOut();
+    _localStorageRepository.setToken('');
+  }
 }
