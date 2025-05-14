358cf2e87657f6f5cabeb27e74fc64e73b825a21
Piyushbhatt7 <vengersmon003@gmail.com>
2025-05-14 09:19:48 +0530

    verified

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -8,7 +8,7 @@ const auth = async (req, res, next) => {
         const token = req.header("x-auth-token");                
   
         if(!token)  
-        {
+        
             return res.status(401).json({msg: "No auth token access denied"});
 
             const verified = jwt.verify(token, "passwordKey"); // 2:14
@@ -17,7 +17,8 @@ const auth = async (req, res, next) => {
                 return res
             .status(401)
             .json({msg: 'Token verification failed, authorization failed'});
-        }
+        
+            req.user = verified.indexOf;
     }
     catch(e)
     {
