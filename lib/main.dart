fa9f56a0958077b819257feb4cf2fe95a64bcdaf
Piyushbhatt7 <vengersmon003@gmail.com>
2025-05-14 10:32:54 +0530

    main

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,27 @@ void main() {
       );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+@override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getUserData();
+
+  }
+
+  void getUserData() async
+  {
+    
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
