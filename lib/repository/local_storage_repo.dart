cf3371e0de5e96af68da64974664564ccfb8a3ba
Piyushbhatt7 <vengersmon003@gmail.com>
2025-05-14 09:41:30 +0530

    local repo

--- lib/repository/local_storage_repo.dart
+++ lib/repository/local_storage_repo.dart
@@ -2,9 +2,15 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class LocalStorageRepo {
 
-  void setToken() async
+  void setToken(String token) async
   {
     SharedPreferences preferences = await SharedPreferences.getInstance();
     preferences.setString('x-auth-token', token);
+  }
+
+  void getToken() async {
+
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    preferences.getString('x-auth-token');
   }
 }
