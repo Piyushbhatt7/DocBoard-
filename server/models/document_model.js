60bdce086a8c25b16f97695226889bf28784d3e5
Piyushbhatt7 <vengersmon003@gmail.com>
2025-05-15 12:42:27 +0530

    Document Screen

--- server/models/document_model.js
+++ server/models/document_model.js
@@ -1,6 +1,18 @@
 const mongoose = required('mongoose');
 const documentSchema = mongoose.Schema({
-
+    uid: {
+        required: true,
+        type: String,
+    },
+    createdAt: {
+        required: true,
+        type: Number,
+    },
+    title: {
+        required: true,
+        type: String,
+        trim: true,
+    }
 });
 
 module.exports = documentSchema;
