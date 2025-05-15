df55031a08832558fd8e0cc5e8db2368d5463c54
Piyushbhatt7 <vengersmon003@gmail.com>
2025-05-15 22:52:46 +0530

    document schema

--- lib/repository/document_repository.dart
+++ lib/repository/document_repository.dart
@@ -1,4 +1,5 @@
 import 'package:google_docs/models/error_model.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:http/http.dart';
 
 class DocumentRepository {
@@ -9,15 +10,52 @@ class DocumentRepository {
     required Client client
     }) : _client = client;
 
-    Future<ErrorModel> createDocument() async [
+    Future<ErrorModel> createDocument() async {
 
-      try{
-
+       try {
+      // First try to sign in silently // 3:10
+      GoogleSignInAccount? user = await _googleSignIn.signInSilently();
+      
+      // If silent sign-in fails, show the popup
+      if (user == null) {
+        user = await _googleSignIn.signIn();
       }
 
-      catch{
-        
+      if (user != null) {
+        final userAcc = UserModel(
+          email: user.email,
+          name: user.displayName ?? '',
+          profilePic: user.photoUrl ?? '',
+          uid: '',
+          token: '',
+        );
+
+        var res = await _client.post(
+          Uri.parse('$host/api/signup'),
+          body: jsonEncode(userAcc.toJson()),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        );
+
+        switch (res.statusCode) {
+          case 200:
+            final newUser = userAcc.copyWith(
+              uid: jsonDecode(res.body)['user']['_id'],
+              token: jsonDecode(res.body)['token'],
+            );
+            error = ErrorModel(error: null, data: newUser);
+            await _localStorageRepository.setToken(newUser.token);
+            break;
+        }
       }
-    ]
+      } catch (e) {
+        error = ErrorModel(
+          error: e.toString(),
+          data: null,
+        );
+      }
+      return error;
+    }
 
 }
