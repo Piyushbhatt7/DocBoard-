04aa87b75086288f5fe49907ffbb9ff22d811489
Piyushbhatt7 <vengersmon003@gmail.com>
2025-05-15 10:56:04 +0530

    creating route path

--- lib/Screens/router.dart
+++ lib/Screens/router.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:google_docs/Screens/home_screen.dart';
 import 'package:google_docs/Screens/login_scree.dart';
 import 'package:routemaster/routemaster.dart';
 
 final loggedOutRoute = RouteMap(routes: {
 
-  '/': (route) => const MaterialPage(child: LoginScreen())
+  '/': (route) => const MaterialPage(child: LoginScreen()),
+});
+
+final loggedInRoute = RouteMap(routes: {
+
+  '/': (route) => const MaterialPage(child: HomeScreen()),
 });
