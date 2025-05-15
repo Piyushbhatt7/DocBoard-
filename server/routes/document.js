438398d66071674441c68f1de6eba6e2bd821cdd
Piyushbhatt7 <vengersmon003@gmail.com>
2025-05-15 13:49:26 +0530

    Document schema

--- server/routes/document.js
+++ server/routes/document.js
@@ -1,4 +1,16 @@
 const express = require('express');
 const Document = require('../modules/document_model');
 const documentRouter = express.Router();
+const auth = require('../middlewares/auth');
 
+
+documentRouter.post('/doc/create', auth, async(req, res) => {
+
+    try{
+
+    }
+    catch(e) 3:01
+    {
+
+    }
+});
