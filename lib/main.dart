0218a62b7640deb2d47ceb5910008c23e3f6bf97
Piyushbhatt7 <vengersmon003@gmail.com>
2025-05-15 10:40:52 +0530

    main dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
- import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_docs/Screens/home_screen.dart';
 import 'package:google_docs/Screens/login_scree.dart';
@@ -7,11 +7,10 @@ import 'package:google_docs/repository/auth_repository.dart';
 
 void main() {
   runApp(
-    
-     ProviderScope(
+    ProviderScope(
       child: MyApp()
-      ),
-      );
+    ),
+  );
 }
 
 class MyApp extends ConsumerStatefulWidget {
@@ -22,36 +21,9 @@ class MyApp extends ConsumerStatefulWidget {
 }
 
 class _MyAppState extends ConsumerState<MyApp> {
-  ErrorModel? errorModel;
-  bool isLoading = true;
-
-  @override
-  void initState() {
-    super.initState();
-    getUserData();
-  }
-
-  void getUserData() async {
-    errorModel = await ref.read(authRepositoryProvider).getUserData();
-    if (errorModel != null && errorModel!.data != null) {
-      ref.read(userProvider.notifier).update((state) => errorModel!.data);
-    }
-    setState(() {
-      isLoading = false;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     final user = ref.watch(userProvider);
-
-    if (isLoading) {
-      return const MaterialApp(
-        home: Scaffold(
-          body: Center(child: CircularProgressIndicator()),
-        ),
-      );
-    }
 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
