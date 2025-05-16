e45afc9448ad5d923e52c6d5521fd6ff6f514257
Piyushbhatt7 <vengersmon003@gmail.com>
2025-05-16 23:01:08 +0530

    auth repo

--- lib/repository/auth_repository.dart
+++ lib/repository/auth_repository.dart
@@ -6,10 +6,15 @@ import 'package:google_docs/models/user_model.dart';
 import 'package:google_docs/repository/local_storage_repo.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:http/http.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 final authRepositoryProvider = Provider(
   (ref) => AuthRepository(
-    googleSignIn: GoogleSignIn(),
+    googleSignIn: GoogleSignIn(
+      clientId: '193875630945-4g80o8vpci8okssnrbhp3sm72dv7bu83.apps.googleusercontent.com',
+      scopes: ['email', 'profile'],
+      serverClientId: '193875630945-4g80o8vpci8okssnrbhp3sm72dv7bu83.apps.googleusercontent.com',
+    ),
     client: Client(),
     localStorageRepository: LocalStorageRepository(),
   ),
@@ -35,15 +40,21 @@ class AuthRepository {
       data: null,
     );
     try {
+      print('Starting Google Sign In process...');
+      
       // First try to sign in silently
       GoogleSignInAccount? user = await _googleSignIn.signInSilently();
+      print('Silent sign in result: ${user?.email ?? 'null'}');
       
       // If silent sign-in fails, show the popup
       if (user == null) {
+        print('Attempting to show sign in popup...');
         user = await _googleSignIn.signIn();
+        print('Popup sign in result: ${user?.email ?? 'null'}');
       }
 
       if (user != null) {
+        print('User signed in successfully: ${user.email}');
         final userAcc = UserModel(
           email: user.email,
           name: user.displayName ?? '',
@@ -52,26 +63,47 @@ class AuthRepository {
           token: '',
         );
 
+        final url = '$host/api/signup';
+        print('Attempting to connect to: $url');
+
         var res = await _client.post(
-          Uri.parse('$host/api/signup'),
+          Uri.parse(url),
           body: jsonEncode(userAcc.toJson()),
           headers: {
             'Content-Type': 'application/json',
+            'Accept': 'application/json',
           },
         );
 
+        print('Response status: ${res.statusCode}');
+        print('Response body: ${res.body}');
+
         switch (res.statusCode) {
           case 200:
+            final responseData = jsonDecode(res.body);
             final newUser = userAcc.copyWith(
-              uid: jsonDecode(res.body)['user']['_id'],
-              token: jsonDecode(res.body)['token'],
+              uid: responseData['user']['_id'],
+              token: responseData['token'],
             );
             error = ErrorModel(error: null, data: newUser);
             await _localStorageRepository.setToken(newUser.token);
             break;
+          default:
+            error = ErrorModel(
+              error: 'Failed to sign up: ${res.body}',
+              data: null,
+            );
+            break;
         }
+      } else {
+        print('Sign in was cancelled or failed');
+        error = ErrorModel(
+          error: 'Sign in was cancelled or failed',
+          data: null,
+        );
       }
     } catch (e) {
+      print('Error during sign in: $e');
       error = ErrorModel(
         error: e.toString(),
         data: null,
