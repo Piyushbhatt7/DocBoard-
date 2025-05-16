f5b9a448551db11d2824172fb1b4650a4466b799
Piyushbhatt7 <vengersmon003@gmail.com>
2025-05-16 23:01:55 +0530

    document model

--- server/routes/document.js
+++ server/routes/document.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const Document = require('../modules/document_model');
+const Document = require('../models/document_model');
 const documentRouter = express.Router();
 const auth = require('../middlewares/auth');
 const e = require('express');
