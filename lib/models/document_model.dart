c6f500ff825881bd14dd206dfc8f510e7af86619
Piyushbhatt7 <vengersmon003@gmail.com>
2025-05-16 10:24:58 +0530

    ?? ''

--- lib/models/document_model.dart
+++ lib/models/document_model.dart
@@ -1,25 +1,30 @@
-import 'dart:convert';
-import 'dart:math';
-
 class DocumentModel {
 
   final String title;
   final String uid;
   final List content;
 
-  factory DocumentModel.fromMap(Map<String, dynamic> map) {
+  factory DocumentModel.fromJson(Map<String, dynamic> json) {
     return DocumentModel(
-      title: map['title'] ?? '',
-      uid: map['uid'] ?? '',
-      content: map['content'] ,
-      createdAt: DateTime.fromMillisecondsSinceEpoch(map['createdAt']),
-      id: map['_id'] as String,
+      title: json['title'] as String,
+      uid: json['uid'] as String,
+      content: json['content'] as List,
+      createdAt: DateTime.parse(json['createdAt'] as String),
+      id: json['id'] as String,
     );
   }
 
-  String toJson() => json.encode(toMap());
-
-  factory DocumentModel.fromJson(String source) => DocumentModel.fromMap(map)
+  Map<String, dynamic> toJson() {
+    return {
+      'title': title,
+      'uid': uid,
+      'content': content,
+      'createdAt': createdAt.toIso8601String(),
+      'id': id,
+    };
+  }
+  final DateTime createdAt;
+  final String id;
 
   DocumentModel({required this.title, required this.uid, required this.content, required this.createdAt, required this.id});
 }
