ca3ee9a7395cbdb1cb942a9db80b358e5a82285f
Piyushbhatt7 <vengersmon003@gmail.com>
2025-05-19 23:51:38 +0530

    router

--- lib/Screens/router.dart
+++ lib/Screens/router.dart
@@ -4,7 +4,7 @@ import 'package:google_docs/Screens/home_screen.dart';
 import 'package:google_docs/Screens/login_scree.dart';
 import 'package:routemaster/routemaster.dart';
 
-final loggedOutRoute = RouteMap(routes: {    
+final loggedOutRoute = RouteMap(routes: {     
 
   '/': (route) => const MaterialPage(child: LoginScreen()),
 });
