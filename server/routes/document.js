d309ecd4ec20007f16c572c38b4f7e181ff15c4d
Piyushbhatt7 <vengersmon003@gmail.com>
2025-05-21 09:29:08 +0530

    document

--- server/routes/document.js
+++ server/routes/document.js
@@ -28,7 +28,8 @@ documentRouter.post('/doc/create', auth, async(req, res) => {
 documentRouter.get('/docs/me', auth, async (req, res) => {
 
     try {
-        let document = await Document.find({uid: })
+        let document = await Document.find({uid: req.user});
+        
     }
 
     catch(e)
