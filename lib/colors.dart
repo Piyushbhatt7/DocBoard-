ccbe9c1556c80aa455342c36fc58efdbedba7b07
Piyushbhatt7 <vengersmon003@gmail.com>
2025-05-24 12:11:36 +0530

    adding blue color to a colors file

--- lib/colors.dart
+++ lib/colors.dart
@@ -3,4 +3,4 @@ import 'package:flutter/material.dart';
 const kWhiteColor = Colors.white;
 const kBlackColor = Colors.black;
 const KredColor = Colors.red;
-const KblueColor = Colors.lightBlue;
+const KblueColor = Color.fromRGBO(26, 115, 232, 1);
