01c2f650b862420ac40e42c227b536523f81cfff
Piyushbhatt7 <vengersmon003@gmail.com>
2025-05-24 22:36:21 +0530

    adding color

--- lib/colors.dart
+++ lib/colors.dart
@@ -4,3 +4,4 @@ const kWhiteColor = Colors.white;
 const kBlackColor = Colors.black;
 const KredColor = Colors.red;
 const KblueColor = Color.fromRGBO(26, 115, 232, 1);
+var KgreyColor = Colors.grey.shade800;
