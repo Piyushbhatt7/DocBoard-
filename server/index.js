167fec20a76558ffc13b0edaeebd5cc062bb283c
Piyushbhatt7 <vengersmon003@gmail.com>
2025-05-26 00:11:36 +0530

    index js

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();    
 
 var server = http.createServer(app);
-var io = require('socket.io')(server);
+var io = require("socket.io")(server);
 
 // Configure CORS for web development
 app.use(cors({
