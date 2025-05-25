9ce54a406df5375fe70c3748234a23fdf5e31560
Piyushbhatt7 <vengersmon003@gmail.com>
2025-05-25 23:42:03 +0530

    socket client

--- lib/clients/socket_client.dart
+++ lib/clients/socket_client.dart
@@ -1,7 +1,13 @@
+import 'dart:io';
+
 import 'package:socket_io_client/socket_io_client.dart' as io;
 
 class SocketClient {
 
   io.Socket? socket;
   static SocketClient? _instance;
+
+  SocketClient._internal() {
+    
+  }
 }
