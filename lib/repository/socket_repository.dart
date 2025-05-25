7b6dc24d11fc0fea7fb86520bd876308a91859f8
Piyushbhatt7 <vengersmon003@gmail.com>
2025-05-26 01:32:23 +0530

    socket repo

--- lib/repository/socket_repository.dart
+++ lib/repository/socket_repository.dart
@@ -10,20 +10,30 @@ class SocketRepository {
   void joinRoom(String documentId)
   {
     print('Joining room: $documentId');
+    if (!_socketClient.connected) {
+      print('Socket not connected, attempting to connect...');
+      _socketClient.connect();
+    }
     _socketClient.emit('join', documentId);
   }
 
   void typing(Map<String, dynamic> data)
   {
+    if (!_socketClient.connected) {
+      print('Socket not connected, attempting to connect...');
+      _socketClient.connect();
+    }
     print('Sending changes to room: ${data['room']}');
+    print('Changes data: ${data['delta']}');
     _socketClient.emit('changes', data);
   }
 
-  void changeListener(Function(Map<String, dynamic> ) func) {
+  void changeListener(Function(Map<String, dynamic>) func) {
     print('Setting up change listener');
-    _socketClient.on('changes', (data)=> {
-      print('Received changes event: $data'),
-      func(data),
+    _socketClient.off('changes'); // Remove any existing listeners
+    _socketClient.on('changes', (data) {
+      print('Received changes event: $data');
+      func(data);
     });
   }
 
