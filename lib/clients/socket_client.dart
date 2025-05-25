ffe922485201dbed2f41d168e92c3d7afae08923
Piyushbhatt7 <vengersmon003@gmail.com>
2025-05-25 23:43:14 +0530

    socket client

--- lib/clients/socket_client.dart
+++ lib/clients/socket_client.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:google_docs/constants.dart';
 import 'package:socket_io_client/socket_io_client.dart' as io;
 
 class SocketClient {
@@ -8,6 +9,8 @@ class SocketClient {
   static SocketClient? _instance;
 
   SocketClient._internal() {
-    
+    socket = io.io(host, <String, dynamic> {
+      
+    });
   }
 }
