2fbd81d3f6c7ac321c6959bbeb44d022207ab668
Piyushbhatt7 <vengersmon003@gmail.com>
2025-05-26 00:11:20 +0530

    socket repo

---
+++ lib/repository/socket_repository.dart
@@ -0,0 +1,9 @@
+import 'package:google_docs/clients/socket_client.dart' show SocketClient;
+import 'package:socket_io_client/src/socket.dart';
+
+class SocketRepository {
+
+  final _socketClient = SocketClient.instance.socket!;
+
+  Socket get socketClient => _socketClient;
+}
