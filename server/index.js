427e8f3f861ce3d91c2427cd6bdbb7712fed6bb2
Piyushbhatt7 <vengersmon003@gmail.com>
2025-06-01 15:29:31 +0530

    index

--- server/index.js
+++ server/index.js
@@ -5,22 +5,21 @@ const cors = require('cors');
 const authRouter = require("./routes/auth");
 const documentRouter = require("./routes/document");
 const http = require('http');
-const { Socket } = require("dgram");
+const { Server } = require("socket.io");
 const Document = require('./models/document_model');
-// const io = require("socket.io")(server, {
-//   cors: {
-//     origin: "*", // You can specify mobile IP or leave it as '*'
-//     methods: ["GET", "POST"]
-//   }
-// });
-
 
 const PORT = process.env.PORT || 3001;      
 
 const app = express();    
 
 var server = http.createServer(app);
-var io = require("socket.io")(server);
+var io = new Server(server, {
+  cors: {
+    origin: ['http://localhost:5000', 'http://127.0.0.1:5000', 'http://localhost:3000','http://localhost:3001', 'http://127.0.0.1:3000'],
+    methods: ["GET", "POST"],
+    credentials: true
+  }
+});
 
 // Configure CORS for web development
 app.use(cors({
